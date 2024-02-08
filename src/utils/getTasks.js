7e1a0c4424f1247364d7a2e53681eccb959ea54c
fahimshariar28 <fahimshariar28@gmail.com>
2024-02-09 02:04:01 +0600

    utility function to get tasks

---
+++ src/utils/getTasks.js
@@ -0,0 +1,23 @@
+const getTasks = (priority, setTasks) => {
+  // get tasks from the local storage
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+  // set tasks based on the priority
+  if (priority === "all") {
+    setTasks(tasks);
+  }
+  if (priority === "low") {
+    const lowPriorityTasks = tasks.filter((task) => task.priority === "low");
+    setTasks(lowPriorityTasks);
+  }
+  if (priority === "medium") {
+    const mediumPriorityTasks = tasks.filter(
+      (task) => task.priority === "medium"
+    );
+    setTasks(mediumPriorityTasks);
+  }
+  if (priority === "high") {
+    const highPriorityTasks = tasks.filter((task) => task.priority === "high");
+    setTasks(highPriorityTasks);
+  }
+};
+export default getTasks;
